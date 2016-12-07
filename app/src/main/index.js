ProFullStackDev/Kap

commit f7ed8606cfed16291c090472d1f4849fb20f08ff

Better cropper border sizing

app/src/renderer/css/cropper.css
@@ -3,7 +3,7 @@
 html, body {
   width: 100%;
   height: 100%;
-  overflow: hidden;
+  /*overflow: hidden;*/
 }
 
 svg {
@@ -27,12 +27,12 @@ svg {
 rect.stroke-black {
   stroke: black;
   fill: none;
-  stroke-width: 3px;
+  stroke-width: 2px;
 }
 
 rect.stroke-white {
   stroke: white;
   fill: none;
-  stroke-dasharray: 3,3;
-  stroke-width: 3px;
+  stroke-dasharray: 4;
+  stroke-width: 2px;
 }

commit 7bac420f862a561240340c0a280dcc37754d78cc

Feature/multiple screens (#93)
* Use the latest `aperture.js` directly from the GitHub repository

* Allow the cropper window to be moved to another display

* Fix: always record the screen in which the cropper is placed on

* Fix a regression introduced by f7ed8606cfed16291c090472d1f4849fb20f08ff

* Fix another regression from f7ed8606cfed16291c090472d1f4849fb20f08ff :sweat_smile:

* Make Circle save the artifacts for any branch

app/src/main/index.js
@@ -26,7 +26,7 @@ const menubar = require('menubar')({
 
 let appState = 'initial';
 let cropperWindow;
-const cropperWindowBuffer = 4;
+const cropperWindowBuffer = 2;
 let mainWindowIsDetached = false;
 let mainWindow;
 let mainWindowIsNew = true;
@@ -119,19 +119,26 @@ ipcMain.on('open-cropper-window', (event, size) => {
 
     cropperWindow.on('moved', () => {
       let [x, y] = cropperWindow.getPosition();
-      const [width, height] = cropperWindow.getSize();
-      const {width: screenWidth, height: screenHeight} = screen.getPrimaryDisplay().bounds;
-      const x2 = x + width;
-      const y2 = y + height;
 
-      if (x < 0 || y < 0 || x2 > screenWidth || y2 > screenHeight) {
-        x = x < 0 ? 0 : x;
-        x = x2 > screenWidth ? screenWidth - width : x;
-        y = y < 0 ? 0 : y;
-        y = y2 > screenHeight ? screenHeight - height : y;
-        cropperWindow.setPosition(x, y, true);
+      // TODO: we need to implement some logic to, at the same time, allow the user
+      // to move the window to another display, but don't allow them to move the window
+      // to ouside of a display. it should be tricky to implement – how can we decide if
+      // a movement is valid – that is, the window is being moved to another display
+      // or it's simply being moved to outside of a display?
+      if (screen.getAllDisplays().length === 1) {
+        const [width, height] = cropperWindow.getSize();
+        const {width: screenWidth, height: screenHeight} = screen.getPrimaryDisplay().bounds;
+        const x2 = x + width;
+        const y2 = y + height;
+
+        if (x < 0 || y < 0 || x2 > screenWidth || y2 > screenHeight) {
+          x = x < 0 ? 0 : x;
+          x = x2 > screenWidth ? screenWidth - width : x;
+          y = y < 0 ? 0 : y;
+          y = y2 > screenHeight ? screenHeight - height : y;
+          cropperWindow.setPosition(x, y, true);
+        }
       }
-
       settings.set('cropperWindow.position', {x, y}, {volatile: true});
     });
   }
@@ -196,11 +203,15 @@ function openPrefsWindow() {
   });
 }
 
+function getCropperWindow() {
+  return cropperWindow;
+}
+
 menubar.on('after-create-window', () => {
   let expectedWindowPosition;
   const currentWindowPosition = {};
   mainWindow = menubar.window;
-  app.kap = {mainWindow, openPrefsWindow, settings};
+  app.kap = {mainWindow, getCropperWindow, openPrefsWindow, settings};
   if (isDev) {
     mainWindow.openDevTools({mode: 'detach'});
   }
@@ -312,16 +323,6 @@ menubar.on('after-create-window', () => {
   initErrorReporter();
   logger.init(mainWindow);
   Menu.setApplicationMenu(applicationMenu);
-});
-
-ipcMain.on('get-cropper-bounds', event => {
-  if (cropperWindow) {
-    event.returnValue = cropperWindow.getContentBounds();
-  }
-});
-
-ipcMain.on('is-cropper-active', event => {
-  event.returnValue = Boolean(cropperWindow);
 });
 
 ipcMain.on('will-start-recording', () => {

app/src/renderer/css/cropper.css
@@ -3,7 +3,7 @@
 html, body {
   width: 100%;
   height: 100%;
-  /*overflow: hidden;*/
+  overflow: hidden;
 }
 
 svg {

app/src/renderer/js/main.js
@@ -16,6 +16,8 @@ import {handleTrafficLightsClicks, isVisible} from '../js/utils';
 
 const aperture = require('aperture.js')();
 
+const {app} = remote;
+
 function setMainWindowSize() {
   const width = document.documentElement.scrollWidth;
   const height = document.documentElement.scrollHeight;
@@ -109,21 +111,38 @@ document.addEventListener('DOMContentLoaded', () => {
     setMainWindowTitle('Getting ready...');
     const past = Date.now();
 
-    let cropperBounds;
-    if (ipcRenderer.sendSync('is-cropper-active')) {
-      cropperBounds = ipcRenderer.sendSync('get-cropper-bounds');
+    const cropperBounds = app.kap.getCropperWindow().getBounds();
+    const display = remote.screen.getDisplayMatching(cropperBounds);
+
+    if (display.id === remote.screen.getPrimaryDisplay().id) {
       // convert the coordinates to cartesian coordinates, which are used by CoreMedia
       cropperBounds.y = screen.height - (cropperBounds.y + cropperBounds.height);
+    } else {
+      // if the cropper window is placed in a display that it's not the main one,
+      // we need to do tome _special_ math to calculate its position
+      const displayBounds = display.bounds;
 
-       // the dashed border is 2px wide
-      cropperBounds.x += 2;
-      cropperBounds.y += 2;
-      cropperBounds.width -= 4;
-      cropperBounds.height -= 4;
+      // when there are more than one display, the bounds that macOS returns for a display
+      // that is not the main one are relative to the main display. consequently, the
+      // bounds of windows in that display are relative to the main display.
+      // we need to make those bounds relative to the display in which the cropper window
+      // is placed in order to aperture.js to work properly
+      cropperBounds.x = Math.abs(displayBounds.x - cropperBounds.x);
+      cropperBounds.y = Math.abs(displayBounds.y - cropperBounds.y);
+
+      // convert the coordinates to cartesian coordinates, which are used by CoreMedia
+      cropperBounds.y = displayBounds.height - (cropperBounds.y + cropperBounds.height);
     }
 
+    // the dashed border is 1px wide
+    cropperBounds.x += 1;
+    cropperBounds.y += 1;
+    cropperBounds.width -= 2;
+    cropperBounds.height -= 2;
+
     aperture.startRecording({
-      cropArea: cropperBounds
+      cropArea: cropperBounds,
+      displayId: display.id
     })
       .then(filePath => {
         recordBtn.attributes['data-state'].value = 'ready-to-stop';
@@ -217,7 +236,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   recordBtn.onclick = function () {
-    if (remote.app.kap.postRecWindow) {
+    if (app.kap.postRecWindow) {
       // we need to keep the window visible to show the shake animation
       // (it'll be auto hidden by `menubar` when the post recording window gain focus)
       ipcRenderer.send('set-main-window-visibility', {

circle.yml
@@ -10,6 +10,6 @@ test:
     - npm run dist
 deployment:
   master:
-    branch: master
+    branch: /.*/
     commands:
       - cp dist/mac/*.dmg $CIRCLE_ARTIFACTS